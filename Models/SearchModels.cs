3bb6de8386333f5bc213946611bfa97cbfbedfaf
Stefan Alexandru <stefanalexandru02@gmail.com>
2025-08-29 13:11:37 +0300

    cleanup

--- Models/SearchModels.cs
+++ Models/SearchModels.cs
@@ -16,36 +16,3 @@ public class SearchResult
     public string Snippet { get; set; } = string.Empty;
     public string Domain { get; set; } = string.Empty;
 }
-
-public class GoogleSearchResponse
-{
-    [JsonPropertyName("items")]
-    public GoogleSearchItem[]? Items { get; set; }
-    
-    [JsonPropertyName("searchInformation")]
-    public GoogleSearchInformation? SearchInformation { get; set; }
-}
-
-public class GoogleSearchItem
-{
-    [JsonPropertyName("title")]
-    public string Title { get; set; } = string.Empty;
-    
-    [JsonPropertyName("link")]
-    public string Link { get; set; } = string.Empty;
-    
-    [JsonPropertyName("snippet")]
-    public string Snippet { get; set; } = string.Empty;
-    
-    [JsonPropertyName("displayLink")]
-    public string DisplayLink { get; set; } = string.Empty;
-}
-
-public class GoogleSearchInformation
-{
-    [JsonPropertyName("totalResults")]
-    public string TotalResults { get; set; } = string.Empty;
-    
-    [JsonPropertyName("searchTime")]
-    public double SearchTime { get; set; }
-}
